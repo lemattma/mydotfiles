e49c96dc995bf15582792a7cc39277f33031a47b
Martin Miranda <martinmirandac@gmail.com>
2025-03-07 10:50:46 +1100

    update first run

--- first-run.sh
+++ first-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/lemattma/mydotfiles/refs/heads/master/first-run.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install zsh zsh-completions git bash alfred arc mas
+brew install zsh zsh-completions git bash alfred arc mas alacritty vscode cursor
 
 mkdir -p ~/Projects
 cd ~/Projects || exit
