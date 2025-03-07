dad3f52b7875285a4eeb6c477e4ca7de41743af8
Martin Miranda <martinmirandac@gmail.com>
2025-03-07 12:09:14 +1100

    updat6e

--- first-run.sh
+++ first-run.sh
@@ -3,16 +3,16 @@
 if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo >> "/Users/$(whoami)/.zprofile"
+  # shellcheck disable=SC2016
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "/Users/$(whoami)/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew install zsh zsh-completions git bash alfred arc mas alacritty visual-studio-code cursor
 
-# if [ ! -d ~/Projects ]; then
-#   mkdir ~/Projects
-# fi
+mkdir -p ~/Projects
 
-# cd ~/Projects || exit
-
-# git clone git@github.com:lemattma/mydotfiles.git
+if [ ! -d ~/Projects/mydotfiles ]; then
+  cd ~/Projects || exit
+  git clone git@github.com:lemattma/mydotfiles.git
+fi
