f31a3db080d2c5e99de498d162e643f89e280557
Martin Miranda <martinmirandac@gmail.com>
2025-03-07 11:16:51 +1100

    update first run

--- first-run.sh
+++ first-run.sh
@@ -4,14 +4,11 @@ if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-
-whoami
-
 echo >> "/Users/$(whoami)/.zprofile"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "/Users/$(whoami)/.zprofile"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# brew install zsh zsh-completions git bash alfred arc mas alacritty vscode cursor
+brew install zsh zsh-completions git bash alfred arc mas alacritty vscode cursor
 
 # if [ ! -d ~/Projects ]; then
 #   mkdir ~/Projects
